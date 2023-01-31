f9dcfb3a51858e59a5585c3ebd4890c872771c39
Roy <jh0292jh@gmail.com>
2023-01-31 14:15:42 -0500

    2

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
                   onPressed: () {},
                   icon: (Icon(
                     Icons.close,
-                    color: Colors.black,
+                    color: Colors.red,
                   )))
             ],
           ),
